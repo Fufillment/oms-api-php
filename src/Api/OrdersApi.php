2186031e1e0691c7ec1d9d8bf1c4408e00eab337
FoxxMD <matt.duncan13@gmail.com>
2015-08-14 14:20:52 -0400

    Implement order creation api method
    Also add in query string functionality for get order

--- src/Api/OrdersApi.php
+++ src/Api/OrdersApi.php
@@ -8,14 +8,26 @@
 
 namespace Fulfillment\OMS\Api;
 
-
 use Fulfillment\OMS\Models\Response\Order;
 
 class OrdersApi extends ApiRequestBase
 {
-    public function getOrder($orderId){
-       $orderJson = $this->makeRequest('get', 'orders/'.$orderId);
-        $order = $this->jsonMapper->map($orderJson, new Order());
+    public function getOrder($orderId, $queryString = null)
+    {
+        $orderJson = $this->makeRequest('get', 'orders/' . $orderId, null, $queryString);
+        $order     = $this->jsonMapper->map($orderJson, new Order());
         return $order;
+    }
+
+    public function createOrder(\Fulfillment\OMS\Models\Request\Order $order, $queryString = null)
+    {
+        if ($this->validateRequests) {
+            $order->validate();
+        }
+
+        $orderJson = $this->makeRequest('post', 'orders', $order, $queryString);
+
+        $returnedOrder = $this->jsonMapper->map($orderJson, new Order());
+        return $returnedOrder;
     }
 }
