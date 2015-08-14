fb69b9b3de4578588b4510acca7988fff65c9b1b
FoxxMD <matt.duncan13@gmail.com>
2015-08-14 14:48:53 -0400

    Implement order list

--- src/Api/OrdersApi.php
+++ src/Api/OrdersApi.php
@@ -12,6 +12,18 @@ use Fulfillment\OMS\Models\Response\Order;
 
 class OrdersApi extends ApiRequestBase
 {
+    public function getOrders($fromDate, $toDate, $queryString = null) {
+
+        if(is_null($queryString)){
+            $queryString = [];
+        }
+        $queryString['fromDate'] = $fromDate;
+        $queryString['toDate'] = $toDate;
+
+        $ordersJson = $this->makeRequest('get', 'orders/', null, $queryString);
+        $orders = $this->jsonMapper->mapArray($ordersJson, [], 'Fulfillment\OMS\Models\Response\Order');
+        return $orders;
+    }
     public function getOrder($orderId, $queryString = null)
     {
         $orderJson = $this->makeRequest('get', 'orders/' . $orderId, null, $queryString);
