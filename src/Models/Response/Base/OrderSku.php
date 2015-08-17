4eb5a9cb2b84d01828e5e69d6cd0dd7b693634d1
FoxxMD <matt.duncan13@gmail.com>
2015-08-17 15:18:34 -0400

    Add a field for Id to a response OrderSku

--- src/Models/Response/Base/OrderSku.php
+++ src/Models/Response/Base/OrderSku.php
@@ -12,6 +12,10 @@ namespace Fulfillment\OMS\Models\Response\Base;
 abstract class OrderSku extends \Fulfillment\OMS\Models\Request\Base\OrderSku
 {
     /**
+     * @var int
+     */
+    public $id;
+    /**
      * @var bool
      */
     public $isValid;
